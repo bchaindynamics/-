8c88a0a30fe26aa1d5bebd80e6612d0a0f9cf7cf
gaurav <gauravtiwari20021995@gmail.com>
2020-12-29 16:19:25 +0530

    auth changes

--- JS/app.js
+++ JS/app.js
@@ -11,61 +11,87 @@
 //     console.log('user',user);
 // }
 
+(function()
+{
+  
+
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAXbhwqpsFQs1EFt23wr-PRqIjH4dlT_fU",
-    authDomain: "sakshya-906f0.firebaseapp.com",
-    projectId: "sakshya-906f0",
-    storageBucket: "sakshya-906f0.appspot.com",
-    messagingSenderId: "433984443244",
-    appId: "1:433984443244:web:07b24ed4238301c44fd949"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
+  apiKey: "AIzaSyAXbhwqpsFQs1EFt23wr-PRqIjH4dlT_fU",
+  authDomain: "sakshya-906f0.firebaseapp.com",
+  databaseURL: "https://sakshya-906f0-default-rtdb.firebaseio.com",
+  projectId: "sakshya-906f0",
+  storageBucket: "sakshya-906f0.appspot.com",
+  messagingSenderId: "433984443244",
+  appId: "1:433984443244:web:07b24ed4238301c44fd949"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebase);
   // get elements 
 
   const firstname = document.getElementById("firstName");
   const lastname = document.getElementById("lastName");
-  const email = document.getElementById("lemail");
-  const password = document.getElementById("passwordInput");
+  const lemail = document.getElementById("email");
+  const password = document.getElementById("password");
+  const btnlogin = document.getElementById("btnlogin");
+  const btnsignup = document.getElementById("btnsignup");
 
+  
   //Add login event 
-  btnlogin.addEventListener('click',e => {
+  // btnlogin.addEventListener('click', e => {
 
     //Get element and pass 
 
-    const email = lemail.value;
-    const pass = passwordInput.value;
-    const auth = firebase.auth();
+  //   const email = lemail.value;
+  //   const pass = passwordInput.value;
+  //   const auth = firebase.auth();
 
-    const promise = auth.signInWithEmailAndPassword(email, password);
-    promise.catch(e =>console.log.(e.message));
+  //   const promise = auth.signInWithEmailAndPassword(email, pass);
+  //   promise.catch(e => console.log(e.message));
 
-  });
+  // });
 
   
   // Add signup event 
   btnsignup.addEventListener('click',e => {
-
+    console.log(lastname.value);
+    e.preventDefault();
     //Get element and pass 
 
     const email = lemail.value;
-    const pass = passwordInput.value;
+    const pass = password.value;
     const auth = firebase.auth();
 
-    const promise = auth.createUserWithEmailAndPassword(email, password);
-    promise.catch(e =>console.log.(e.message));
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e =>console.log(e.message));
 
   });
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((user) => {
-    // Signed in 
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
+
+// Add a real time listner 
+      const auth = firebase.auth();
+  firebase.auth().onAuthStateChanged( firebaseUser => {
+        if (firebaseUser) 
+        {
+          console.log(firebaseUser);
+        }
+          else
+          {
+            console.log('not loged in');
+          }
+    });
+
+
+  // firebase.auth().signInWithEmailAndPassword(email, password)
+  // .then((user) => {
+  //   // Signed in 
+  //   // ...
+  // })
+  // .catch((error) => {
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  // });
+
+}());
