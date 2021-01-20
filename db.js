ea840c724bfc5aabdc8087c685bc5c5e3cd77b7a
gaurav <gauravtiwari20021995@gmail.com>
2021-01-20 21:05:50 +0530

    DB connections

---
+++ db.js
@@ -0,0 +1,17 @@
+// import { connect } from 'mongoose';
+// var url = "mongodb://localhost:27017/userlog";
+
+// connect(url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Database created!");
+//   db.close();
+// });
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb1";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
