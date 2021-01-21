ea3c1400419e17817b173401cf74f163f85c7eb5
gauravtwr <gauravtiwari20021995@gmail.com>
2021-01-21 23:54:00 +0530

    DB connection setup succesfull

--- db.js
+++ db.js
@@ -1,17 +1,29 @@
-// import { connect } from 'mongoose';
-// var url = "mongodb://localhost:27017/userlog";
-
-// connect(url, function(err, db) {
-//   if (err) throw err;
-//   console.log("Database created!");
-//   db.close();
-// });
-
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb1";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connectd");
+  setup();
 });
+
+function setup(){
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+  const Kitten = mongoose.model('Kitten', kittySchema);
+  const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+  silence.save(err => {
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log("log created");
+    }
+  })
+
+}
+
+
+
